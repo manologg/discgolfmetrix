6ceb0308aaa6bac52158d1215427119d470ad762
Manuel García García <marianocucufate@gmail.com>
2023-09-21 11:27:32 +0200

    Update tremonia-putting-liga-poc.js

--- tremonia-putting-liga-poc.js
+++ tremonia-putting-liga-poc.js
@@ -7,8 +7,8 @@ function invertTable(tbody) {
   tbody.html($('tr',tbody).get().reverse());
 }
 
-function hideColumns(trContainer, columnSelectors) {
-  columnSelectors.forEach(selector => trContainer.find(`tr td:${selector}`).hide());
+function hideColumns(trContainer, columnType, columnSelectors) {
+  columnSelectors.forEach(selector => trContainer.find(`tr ${columnType}:${selector}`).hide());
 }
 
 
@@ -20,7 +20,7 @@ tbody = $("#id_results tbody:last()");
 thead = $("#id_results thead:last()");
 uselessColumns = ['nth-child(3)', 'nth-last-child(2)'];
 
-hideColumns(tbody, uselessColumns);
-hideColumns(thead, uselessColumns);
+hideColumns(tbody, 'td', uselessColumns);
+hideColumns(thead, 'th', uselessColumns);
 
 invertTable(tbody);
