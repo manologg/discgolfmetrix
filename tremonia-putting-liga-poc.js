6552850251a06f5050f46a185c32f7042cd763d6
Manuel García García <marianocucufate@gmail.com>
2023-10-04 11:19:24 +0200

    Update tremonia-putting-liga-poc.js

--- tremonia-putting-liga-poc.js
+++ tremonia-putting-liga-poc.js
@@ -1,7 +1,7 @@
 /* CONSTANTS */
 
 var REPO_BASE_URL = "https://raw.githubusercontent.com/manologg/discgolfmetrix/main/";
-var VERSION = '11:12';
+var VERSION = '11:19';
 console.log(VERSION);
 var DEBUG = (typeof DEBUG !== "undefined") && DEBUG
 
