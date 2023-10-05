bbfd3d42dda81595ce73df54764526e6744ea4a8
Manuel García García <marianocucufate@gmail.com>
2023-10-05 10:13:01 +0200

    Update putting-league.js

--- putting-league.js
+++ putting-league.js
@@ -1,7 +1,7 @@
 /***** CONSTANTS *****/
 
 var REPO_BASE_URL = "https://raw.githubusercontent.com/manologg/discgolfmetrix/main/";
-var VERSION = '10:09';
+var VERSION = '10:12';
 console.log(VERSION);
 var DEBUG = (typeof DEBUG !== "undefined") && DEBUG
 
