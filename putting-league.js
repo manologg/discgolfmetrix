3b6a9110ed468d980ff67405f18bf89c5426e3e6
Manuel García García <marianocucufate@gmail.com>
2023-10-05 10:09:27 +0200

    Update putting-league.js

--- putting-league.js
+++ putting-league.js
@@ -1,7 +1,7 @@
-/* CONSTANTS */
+/***** CONSTANTS *****/
 
 var REPO_BASE_URL = "https://raw.githubusercontent.com/manologg/discgolfmetrix/main/";
-var VERSION = '09:53';
+var VERSION = '10:09';
 console.log(VERSION);
 var DEBUG = true; //(typeof DEBUG !== "undefined") && DEBUG
 
@@ -13,33 +13,42 @@ var LEAGUE = 0;
 var TOURNAMENT = 1;
 var ROUND = 2;
 
-/* THESE CAN BE DEFINED IN METRIX */
+// just for debug
+DEFAULT = 'DEFAULT';
+METRIX = 'configured in metrix';
+
+/***** THESE CAN BE DEFINED IN METRIX *****/
 
 if (typeof POINT_SYSTEM === 'undefined') {
   POINT_SYSTEM = {1: 1, 2: 1, 3: 1, 4: 1, 5: 1};
   DEFAULT_POINTS = true;
-  console.log('Using DEFAULT point system:', POINT_SYSTEM);
+  origin = DEFAULT;
 }
 else {
   DEFAULT_POINTS = false;
-  console.log('Using point system:', POINT_SYSTEM);
+  origin = METRIX;
 }
+console.log(`[${origin}] Using point system:`, POINT_SYSTEM);
 
 if (typeof MAX_PUTTS_PER_STATION === 'undefined') {
   MAX_PUTTS_PER_STATION = 3;
-  console.log('Only 3 putts per station allowed (DEFAULT)');
+  origin = DEFAULT;
 }
 else {
-  console.log(`Only ${MAX_PUTTS_PER_STATION} putts per station allowed`);
+  origin = METRIX;
 }
+console.log(`[${origin}] Only ${MAX_PUTTS_PER_STATION} putts per station allowed`);
 
 if (typeof EXTRA_POINTS_IF_ALL_PUTTS_ARE_MADE === "undefined") {
   EXTRA_POINTS_IF_ALL_PUTTS_ARE_MADE = 0;
-  console.log('No extra points if all putts from one station are made (DEFAULT)');
+  origin = DEFAULT;
 }
 else {
-  console.log(`If all putts from one station are made the player gets ${EXTRA_POINTS_IF_ALL_PUTTS_ARE_MADE} extra points (DEFAULT)`);
+  origin = METRIX;
 }
+console.log(`[${origin}] If all putts from one station are made the player gets ${EXTRA_POINTS_IF_ALL_PUTTS_ARE_MADE} extra points`);
+
+/***** THOUSAND FUNCTIONS *****/
 
 function loadCss() {
   $.ajax({
@@ -170,45 +179,45 @@ function setTrSumAndOrder(i, tr) {
 function setTrPosition(i, tr) {
     
   lastTr = $(tr).prev();
-    sum = getSum(tr);
-    lastSum = getSum(lastTr);
-    lastPosition = getPosition(lastTr);
+  sum = getSum(tr);
+  lastSum = getSum(lastTr);
+  lastPosition = getPosition(lastTr);
+  
+  if (currentCompetition === LEAGUE || currentCompetition === ROUND) {
     
-    if (currentCompetition === LEAGUE || currentCompetition === ROUND) {
-      
+    if (sum == lastSum) {
+      position = lastPosition;
+    }
+    else {
+      position = i+1;
+    }
+  }
+  // currentCompetition === TOURNAMENT
+  else if (i%2 == 1) { // even rows
+      position = lastPosition;
+  }
+  else { // odd rows
       if (sum == lastSum) {
         position = lastPosition;
       }
       else {
-        position = i+1;
+        position = i/2+1;
       }
-    }
-    // currentCompetition === TOURNAMENT
-    else if (i%2 == 1) { // even rows
-        position = lastPosition;
-    }
-    else { // odd rows
-        if (sum == lastSum) {
-          position = lastPosition;
-        }
-        else {
-          position = i/2+1;
-        }
-    }
-    if (DEBUG) {
-      console.log('tr', tr);
-      console.log('lastTr', lastTr[0]);
-      console.log('sum', sum);
-      console.log('lastSum', lastSum);
-      console.log('lastPosition', getPosition(lastTr));
-      console.log(`i: ${i}, position: ${position}`);
-      console.log('-----------------------------------------')
-    }
-    setData(tr, 'position', position);
-    if (currentCompetition === LEAGUE || currentCompetition === ROUND || i%2 == 0) { // ONLY odd rows in TOURNAMENT competition
-      $(tr).find('td:first()').text(position);
-    }
   }
+  if (DEBUG) {
+    console.log('tr', tr);
+    console.log('lastTr', lastTr[0]);
+    console.log('sum', sum);
+    console.log('lastSum', lastSum);
+    console.log('lastPosition', getPosition(lastTr));
+    console.log(`i: ${i}, position: ${position}`);
+    console.log('-----------------------------------------')
+  }
+  setData(tr, 'position', position);
+  if (currentCompetition === LEAGUE || currentCompetition === ROUND || i%2 == 0) { // ONLY odd rows in TOURNAMENT competition
+    $(tr).find('td:first()').text(position);
+  }
+}
 
 function hideFirstTable() {
   $('#id_results tbody:first()').hide();
@@ -228,45 +237,47 @@ function hideColumns(trContainer, columnType, columnSelectors) {
 }
 
 function displayThPoints(i, th) {
-    $(th).text(`${$(th).text()} (${POINT_SYSTEM[i+1]}P)`)
+  $(th).text(`${$(th).text()} (${POINT_SYSTEM[i+1]}P)`)
 }
 
-/* MAIN */
+function customizeResultsTable() {
+  var thead = $('#id_results thead:last()');
+  var tbody = $('#id_results tbody:last()');
+  
+  removeColors(tbody);
+  
+  var uselessColumns;
+  if (currentCompetition === TOURNAMENT || currentCompetition === LEAGUE) {
+    uselessColumns = ['nth-child(3)', 'nth-child(4)', 'nth-child(5)', 'nth-last-child(4)', 'nth-last-child(2)'];
+  }
+  else if (currentCompetition === ROUND) {
+    uselessColumns = ['nth-child(3)', 'nth-child(4)', 'nth-last-child(2)'];
+  }
+  
+  hideColumns(tbody, 'td', uselessColumns);
+  hideColumns(thead, 'th', uselessColumns);
+  
+  var uselessElements = ["h2", "#hs-switch-1", "#hs-switch-2", "#hs-switch-3", "#hole-stats-charts-container"]
+  uselessElements.forEach(selector => $(selector).hide());
+  
+  if (!DEFAULT_POINTS) {
+    thead.find('th')
+         .slice(stationsStart, stationsStart + Object.values(POINT_SYSTEM).length)
+         .each(displayThPoints);
+  }
+  
+  tbody.find('tr')
+       .each(setTdSums)
+       .each(setTrSumAndOrder)
+       .sort((a, b) => getOrder(b) - getOrder(a))
+       .each((i, tr) => $(tr).appendTo(tbody))
+       .each(setTrPosition);
+}
+
+/***** MAIN *****/
 
 loadCss();
-
 hideFirstTable();
-
-var thead = $('#id_results thead:last()');
-var tbody = $('#id_results tbody:last()');
-
-removeColors(tbody);
-
-var uselessColumns;
-if (currentCompetition === TOURNAMENT || currentCompetition === LEAGUE) {
-  uselessColumns = ['nth-child(3)', 'nth-child(4)', 'nth-child(5)', 'nth-last-child(4)', 'nth-last-child(2)'];
-}
-else if (currentCompetition === ROUND) {
-  uselessColumns = ['nth-child(3)', 'nth-child(4)', 'nth-last-child(2)'];
-}
-
-hideColumns(tbody, 'td', uselessColumns);
-hideColumns(thead, 'th', uselessColumns);
-
-var uselessElements = ["h2", "#hs-switch-1", "#hs-switch-2", "#hs-switch-3", "#hole-stats-charts-container"]
-uselessElements.forEach(selector => $(selector).hide());
-
-if (!DEFAULT_POINTS) {
-  thead.find('th')
-       .slice(stationsStart, stationsStart + Object.values(POINT_SYSTEM).length)
-       .each(displayThPoints);
-}
-
-tbody.find('tr')
-     .each(setTdSums)
-     .each(setTrSumAndOrder)
-     .sort((a, b) => getOrder(b) - getOrder(a))
-     .each((i, tr) => $(tr).appendTo(tbody))
-     .each(setTrPosition);
+customizeResultsTable();
 
 console.log(VERSION);
